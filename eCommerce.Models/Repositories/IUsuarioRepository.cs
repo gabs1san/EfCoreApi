e06ad38a4e95068c6d17801eda3b0d15dca9b123
gabs1san <sanbiel398@gmail.com>
2025-10-27 20:21:22 -0300

    Interface e padrão Repository

---
+++ eCommerce.Models/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> Get();
+        Usuario Get(int id);
+        void Add(Usuario usuario);
+        void Update(Usuario usuario);  
+        void Delete(int id);
+
+    }
+}
