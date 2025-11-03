bf71377283dbe2c22b0939c11583dad446c38f1a
gabs1san <sanbiel398@gmail.com>
2025-11-03 17:59:26 -0300

    Arrumando a referência e aplicando novamente o padrão repository

---
+++ EfCore.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.Models;
+
+namespace EfCore.API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> Get();
+
+        Usuario GetUsuario();
+
+        void Add(Usuario usuario);
+
+        void Update(Usuario usuario);
+
+        void Delete(int id);
+    }
+}
---
+++ EfCore.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.Models;
+
+namespace EfCore.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        public List<Usuario> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario GetUsuario()
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
