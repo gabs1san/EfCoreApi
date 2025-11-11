a0a8ddb0e082d08fa67294d84666458743ac8c9e
gabs1san <sanbiel398@gmail.com>
2025-11-11 18:59:27 -0300

    Aprendendo mapemento de banco de dados usando o DataAnnotations

---
+++ eCommerce.Models.DataAnnotations/Contato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+
+       //[ForeignKey(“Usuario”)]
+        
+        public string? Telefone { get; set; }
+        public string? Celular { get; set; }
+
+        
+        public int UsuarioId { get; set; }
+
+        //[ForeignKey("UsuarioId")]
+        public Usuario? Usuario { get; set; }
+
+    }
+}
---
+++ eCommerce.Models.DataAnnotations/Usuario.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace eCommerce.Models
+{
+    [Index(nameof(Email), IsUnique = true , Name = "IX_EMAIL_UNICO")]
+    [Index(nameof(Nome), nameof(CPF))]
+    [Table("TB_USUARIOS")]
+    public class Usuario
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Sexo { get; set; }
+
+        [Column("REGISTRO_GERAL")]
+        public string? RG { get; set; }
+
+        public string CPF { get; set; } = null!;
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Matricula { get; set; }
+
+        [NotMapped]
+        public bool RegistroAtivo { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTimeOffset DataCadastro { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public Contato? Contato { get; set; }
+
+        public ICollection<EnderecoEntrega>? EnderecosEntregas { get; set; }
+        public ICollection<Departamento>? Departamentos { get; set; }
+
+
+        [InverseProperty("Cliente")]
+        public ICollection<Pedido>? PedidosCompradosPeloCliente { get; set; }
+
+        [InverseProperty("Colaborador")]
+        public ICollection<Pedido>? PedidosGerenciadosPeloColaborador { get; set; }
+
+        [InverseProperty("Supervisor")]
+        public ICollection<Pedido>? PedidosSupervisionadosPeloColaboradorSupervisor { get; set; }
+
+
+    }
+}
